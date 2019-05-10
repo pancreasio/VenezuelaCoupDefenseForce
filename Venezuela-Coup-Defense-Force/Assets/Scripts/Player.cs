fd759e06969f19c496f74e43a04ab3411bff6e9d
pancreasio <julianholisu@gmail.com>
2019-05-10 17:43:08 -0300

    working base and cannon rotation

--- Venezuela-Coup-Defense-Force/Assets/Scripts/Player.cs
+++ Venezuela-Coup-Defense-Force/Assets/Scripts/Player.cs
@@ -4,31 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Ray ray;
-    RaycastHit hit;
-    GameObject target;
-    Transform targetPos;
+
+    private Ray ray;
+    private RaycastHit hit;
+    private GameObject target;
+    private Vector3 baseTargetPos;
+    private Vector3 cannonTargetPos;
+    private Transform baseTargetRotation;
+    private Transform cannonTargetRotation;
+
+    public float baseRotationSpeed;
+    public float cannonRotationSpeed;
+    public Transform chamber;
 
     private void Start()
     {
         target = null;
+        baseTargetRotation = new GameObject().transform;
+        cannonTargetRotation = new GameObject().transform;
     }
 
     private void Update()
     {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit) && hit.transform.gameObject != null)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 target = hit.transform.gameObject;
-                targetPos = hit.transform;
+                baseTargetPos = new Vector3(hit.transform.position.x, 0, hit.transform.position.z);
+                cannonTargetPos = new Vector3(hit.transform.position.x, hit.transform.position.y, hit.transform.position.z);
                 Debug.Log(hit.collider.name);
                 Debug.DrawRay(ray.origin, ray.direction * Vector3.Distance(ray.origin, hit.transform.position), Color.red, 1);
             }
-            if (target != null)
+        }
+        if (target != null)
+        {
+            baseTargetRotation.transform.LookAt(baseTargetPos);
+            cannonTargetRotation.transform.LookAt(cannonTargetPos);
+            transform.rotation = Quaternion.Lerp(transform.rotation, baseTargetRotation.rotation, baseRotationSpeed * Time.deltaTime);
+            if (transform.rotation == baseTargetRotation.rotation|| Quaternion.Angle(transform.rotation, baseTargetRotation.rotation) <3)
             {
-              
+                chamber.rotation = Quaternion.Lerp(chamber.rotation, cannonTargetRotation.rotation, cannonRotationSpeed * Time.deltaTime);
             }
         }
     }
